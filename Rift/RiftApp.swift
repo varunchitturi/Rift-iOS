c129222dfd6cd289191b127c024d7f99e019504f
Varun Chitturi <varunchitturi@icloud.com>
2021-08-09 19:45:10 -0700

    Initial Commit

---
+++ Rift/RiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiftApp.swift
+//  Rift
+//
+//  Created by Varun Chitturi on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct RiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
