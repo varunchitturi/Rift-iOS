f6cc2887b9f9051f91a35e4c3cfd40920fac0adc
School Scope Admin <admin@schoolscope.org>
2022-02-10 12:31:08 -0800

    Add documentation for GradingCategory

--- Rift/API/Models/GradingCategory.swift
+++ Rift/API/Models/GradingCategory.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+/// A category used in calculating the final grade in a term
+/// - A `GradingCategory` contains the grade, assignments, and other metadata for a category
 struct GradingCategory: Identifiable, Decodable, Equatable {
     
     init(id: Int, name: String, isWeighted: Bool, weight: Double, isExcluded: Bool, isCalculated: Bool = false, assignments: [Assignment], usePercent: Bool, categoryGrade: CategoryGrade? = nil) {
@@ -21,16 +23,50 @@ struct GradingCategory: Identifiable, Decodable, Equatable {
         self.categoryGrade = categoryGrade
     }
     
+    /// An Infinite Campus generated `ID` for a category
+    /// - Equivalent to `groupID` in the official Infinite Campus API
     let id: Int
+    
+    /// The name of the category
     let name: String
+    
+    /// Gives whether this category is weighted in terms of grade calculation
+    /// - If `false`, then this category doesn't use weight-style grade calculation
+    /// - If a term, doesn't use weight-style grade calculation, then all of it categories will have `isWeighted` set to `false`
     let isWeighted: Bool
+    
+     /// If the term uses a weighted grading style, then the `weight` is how much this category is weighted
+     /// - The weight is given as a whole number.
+     /// - Note: If `isWeighted` is false, this value will be 0
     let weight: Double
+    
+    /// Gives whether this category should be excluded when calculating the grade for a term
     let isExcluded: Bool
+    
+    /// Gives whether the current points should be calculated manually using the grades for each assignment in `assignments` or whether the default value from Infinite Campus is used
+    /// - If `false` the default value from Infinite Campus will be used
+    /// - If `true` the `currentPoints`, `totalPoints`, and `percentage` will be calculated manually by iterating over all the assignments
+    /// - Note: `currentPoints`, `totalPoints`, and `percentage` are computed values and are reactive to changes to `assignments` when this is `true`
+    ///
+    /// \
+    ///   The default value of `isCalculated` is `false`
     var isCalculated = false
+    
+    /// The assignments that are in this category
     var assignments: [Assignment]
+    
+    /// Gives whether the percentage or points of assignments  should be used in grade calculation
+    /// - If `true`, then the number of points for assignments won't be considered. Only the ratio between scored points over total points (percentage) will be sued to calculated the grade. For example. an assignment that is scored as 1/2 has the same affect on your grade as an assignment that is scored as 50/100.
+    /// - If `false`, then the number of points for assignment will be considered. For example, an assignment that is scored as 1/2 will affect your grade less than an assignment that is scored as 50/100.
     let usePercent: Bool
+    
+    /// The grade for this category provided by Infinite Campus
+    /// - If `isCalculated` is `true`, then the values in `categoryGrade` will be disregarded and the grades will be calculated manually from `assignments`
+    /// - If `isCalculated` is `false`, then the values in `categoryGrade` will be used
     private(set) var categoryGrade: CategoryGrade?
     
+    /// The total points earned from all assignments in this category
+    /// - Note: Calculation of this value is affected by `isCalculated`
     var currentPoints: Double? {
         if isCalculated {
             var currentPoints: Double?
@@ -45,6 +81,8 @@ struct GradingCategory: Identifiable, Decodable, Equatable {
         return categoryGrade?.currentPoints
     }
     
+    /// The total points from all assignments in this category
+    /// - Note: Calculation of this value is affected by `isCalculated`
     var totalPoints: Double? {
         if isCalculated {
             var totalPoints: Double?
@@ -59,6 +97,8 @@ struct GradingCategory: Identifiable, Decodable, Equatable {
         return categoryGrade?.totalPoints
     }
     
+    /// The final grade percentage of this category
+    /// - Note: Calculation of this value is affected by `isCalculated`
     var percentage: Double? {
         if isCalculated {
             guard let currentPoints = currentPoints, let totalPoints = totalPoints else {
