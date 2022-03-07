e7b0dd094488b16345156c728e092c7af5c8a81e
Varun Chitturi <varunchitturi@icloud.com>
2022-03-07 12:09:46 -0800

    Documentation for course detail view

--- Rift/View Models/AssignmentsViewModel.swift
+++ Rift/View Models/AssignmentsViewModel.swift
@@ -8,11 +8,18 @@
 import Foundation
 import SwiftUI
 
+/// MVVM view model for the `AssignmentsView`
 class AssignmentsViewModel: ObservableObject {
     // TODO: edit this to support multiple filters
+    
+    /// MVVM model
     @Published private var assignmentsModel = AssignmentsModel()
+    
+    /// `AsyncState` to manage network calls in views
     @Published var networkState: AsyncState = .idle
     
+    /// A dictionary where each key is a `Date` and each value is a list of assignments that are due on that date
+    /// - A key of `nil` is for assignments that don't have a due date
     var assignmentDateList: [Date?: [Assignment]] {
         var assignmentDateList = [Date?: [Assignment]]()
         for `assignment` in assignmentsModel.assignmentList {
@@ -26,6 +33,7 @@ class AssignmentsViewModel: ObservableObject {
         return assignmentDateList
     }
     
+    /// A sorted list of all the dates that assignments are due on
     var dates: [Date?] {
         assignmentDateList.keys.sorted { lhs, rhs in
             if let lhs = lhs, let rhs = rhs {
@@ -39,6 +47,7 @@ class AssignmentsViewModel: ObservableObject {
         fetchAssignments()
     }
     
+    /// Fetches assignments from the API
     func fetchAssignments() {
         networkState = .loading
         API.Assignments.getList {[weak self] result in
--- Rift/View Models/CourseDetailViewModel.swift
+++ Rift/View Models/CourseDetailViewModel.swift
@@ -9,19 +9,29 @@ import Foundation
 import SwiftUI
 import Algorithms
 
+/// MVVM view model for the `CourseDetailView`
 class CourseDetailViewModel: ObservableObject {
-
+    
+    /// MVVM model
     @Published private var courseDetailModel: CourseDetailModel
+    
+    /// An array grade details that the user can make changes to
     @Published var editingGradeDetails: [GradeDetail]?
+    
+    /// The index of the chosen `GradeDetail` in `courseDetailModel.gradeDetails`
     @Published var chosenGradeDetailIndex: Int?
+    
+    /// `AsyncState` to manage network calls in views
     @Published var networkState: AsyncState = .loading
 
     // TODO: make this process more efficient
-
+    
+    /// The name of the `Course` that is being detailed
     var courseName: String {
         courseDetailModel.course.name
     }
-
+    
+    /// The names of the various `GradeDetail`s the user can view
     var gradeDetailOptions: [String] {
         guard let gradeDetails = courseDetailModel.gradeDetails else {
             return []
@@ -31,14 +41,17 @@ class CourseDetailViewModel: ObservableObject {
             "\($0.grade.termName) \( $0.grade.termType)"
         }
     }
-
+    
+    /// The chosen `GradeDetail` based on `chosenGradeDetailIndex`
     var gradeDetail: GradeDetail? {
         guard let chosenGradeDetailIndex = chosenGradeDetailIndex, courseDetailModel.gradeDetails?.isEmpty == false else {
             return nil
         }
         return courseDetailModel.gradeDetails?[chosenGradeDetailIndex]
     }
-
+    
+    /// A copy of `gradeDetail` that the user can edit
+    /// - This is the `GradeDetail` whose values are reflected in the view
     var editingGradeDetail: GradeDetail? {
         get {
             guard let chosenGradeDetailIndex = chosenGradeDetailIndex, editingGradeDetails?.isEmpty == false else {
@@ -53,15 +66,21 @@ class CourseDetailViewModel: ObservableObject {
             editingGradeDetails?[chosenGradeDetailIndex] = newValue
         }
     }
-
+    
+    /// The letter grade of the chosen `GradeDetail`
+    /// - This value is always the same as the original `GradeDetail`
+    /// - Does not change based on `editingGradeDetail`
     var courseGradeDisplay: String {
         String(displaying: gradeDetail?.grade.letterGrade)
     }
-
+    
+    /// A boolean value that gives if the user has made any changes to the `GradeDetail`
+    /// - Checks if whether all the assignments in `editingGradeDetail` are the same as the one is `gradeDetail`
     var hasModifications: Bool {
         editingGradeDetail?.assignments != gradeDetail?.assignments
     }
-
+    
+    /// Checks if the chosen `GradeDetail` is `nil`
     var hasGradeDetail: Bool {
         gradeDetail != nil
     }
@@ -70,7 +89,8 @@ class CourseDetailViewModel: ObservableObject {
         self.courseDetailModel = CourseDetailModel(course: course, termSelectionID: termSelectionID)
         fetchGradeDetails()
     }
-
+    
+    /// Fetches grade details from the API
     func fetchGradeDetails() {
         API.Grades.getGradeDetails(for: courseDetailModel.course) {[weak self] result in
 
@@ -100,7 +120,10 @@ class CourseDetailViewModel: ObservableObject {
             }
         }
     }
-
+    
+    /// Gets the index of the current `GradeDetail` based on the date
+    /// - Parameter terms: The terms that each `GradeDetail` is of
+    /// - Returns: The index of the current `GradeDetail`
     private func getCurrentGradeDetailIndex(from terms: [Term]) -> Int? {
 
         let currentDate = Date()
@@ -114,7 +137,10 @@ class CourseDetailViewModel: ObservableObject {
 
 
     // MARK: - Intents
-
+    
+    /// Gets the original, unedited version of an `Assignment` based on its `id`
+    /// - Parameter assignment: The `Assignment` that you are trying to get the unedited version of
+    /// - Returns: The original, unedited `Assignment`
     func getOriginalAssignment(for assignment: Assignment) -> Assignment? {
         for originalAssignment in gradeDetail?.assignments ?? [] {
             if originalAssignment.id == assignment.id {
@@ -124,15 +150,19 @@ class CourseDetailViewModel: ObservableObject {
         return nil
     }
     // TODO: consolidate between the word changes and modifications. Both should not be used.
+    /// Resets any changes the user made to `editingGradeDetail`
     func resetChanges() {
         editingGradeDetail = gradeDetail
         editingGradeDetail?.isCalculated = true
     }
-
+    
+    /// Refreshes the view
     func rebuildView() {
         objectWillChange.send()
     }
-
+    
+    /// Deletes an `Assignment` from the assignment list of `editingGradeDetail`
+    /// - Parameter assignment: The `Assignment` to delete
     func deleteAssignment(_ assignment: Assignment) {
         editingGradeDetail?.assignments.removeAll(where: {$0.id == assignment.id})
     }
